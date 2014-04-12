cf605ac277161d435ebf7e4c5dae09d591947483
Michael Blais Younkin <younkin.michael@gmail.com>
2014-04-12 02:57:26 -0400

    Add init() function

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include <ncurses.h>
 
+int init();
+
 int main() {
-    initscr();
+    if (init()) {
+        return -1;
+    }
     printw("Hello world !!!");
     refresh();
     getch();
     endwin();
     return 0;
+}
+
+int init() {
+    if (initscr() == NULL) {
+        fprintf(stderr, "initscr failed");
+        goto init_err;
+    }
+    if (raw() == ERR) {
+        fprintf(stderr, "setting raw mode failed");
+        goto init_err;
+    }
+    if (noecho() == ERR) {
+        fprintf(stderr, "setting noecho mode failed");
+        goto init_err;
+    }
+    return 0;
+init_err:
+    return -1;
 }
